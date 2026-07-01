b66dccea543e2ecb7f96944927f89f4ee723bd99
Pacharchon Gaewpusri <pacharachongaewpusri@gmail.com>
2026-07-01 09:25:58 +0700

    fix app.jsx

--- Front_end/src/App.jsx
+++ Front_end/src/App.jsx
@@ -1,10 +1,27 @@
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { useState } from 'react'
+import './App.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import RootLayout from './layout/RootLayout'
+import Dashboard from './pages/Dashboard'
 
-export default function App() {
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Route>
+    ),
+  )
+
   return (
-    <div>
-      <h1>Curriculum Guide</h1>
-      <p>Welcome to your React application!</p>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
+
+export default App
--- Front_end/src/main.jsx
+++ Front_end/src/main.jsx
@@ -2,33 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-import { useState } from 'react'
-import './App.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import RootLayout from './layout/RootLayout'
-import Dashboard from './pages/Dashboard'
-
-
-function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<RootLayout />}>
-        <Route path='/dashboard' element={<Dashboard />} />
-      </Route>
-    ),
-  )
-
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
