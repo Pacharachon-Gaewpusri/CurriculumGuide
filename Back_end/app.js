d2fd6d8fff1896497308c053ad8d737684f8d9ff
Pacharchon Gaewpusri <pacharachongaewpusri@gmail.com>
2026-07-14 15:52:56 +0700

    moved project again

--- Back_end/app.js
+++ Back_end/app.js
@@ -1,10 +1,25 @@
-const app = App();
-function App() {
-    return (
-        <div>
-            <h1>Curriculum Guide</h1>
-            <p>Default app loaded successfully.</p>
-        </div>
-    );
-}
+// import '../Front_end/src/App.jsx'
+
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
+// app.use("/upload", express.static('src/upload'))
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+
 export default app;
--- Front_end/my-app/App.jsx
+++ Front_end/my-app/App.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import RootLayout from './layout/RootLayout.jsx'
-import Dashboard from './pages/Dashboard.jsx'
+import RootLayout from './src/layout/RootLayout.jsx'
+import Dashboard from './src/pages/Dashboard.jsx'
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
