962d43ae9fc9ba4ad6eadf2cc484970df53a6394
Pacharchon Gaewpusri <pacharachongaewpusri@gmail.com>
2026-07-27 11:27:23 +0700

    reverting structure tocheck for additional errors

--- Front_end/my-app/src/App.jsx
+++ Front_end/my-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
-// import './index.css'
+import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import RootLayout from './layout/RootLayout.jsx'
 import Dashboard from './pages/Dashboard.jsx'
